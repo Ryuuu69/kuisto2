558e5d733a9728964f276efb74108b2eabd6d13e
Ryuuu69 <144459226+Ryuuu69@users.noreply.github.com>
2025-07-27 17:57:45 +0200

    Updated App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,12 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import ProductList   from './ProductList';
-import ProductDetail from './ProductDetail';   // ← (point 2)
+import ProductList from './ProductList';
+import ProductDetail from './ProductDetail';
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/"element={<ProductList />} />
-        {/* UNE SEULE page détail, paramètre :id */}
+        <Route path="/" element={<ProductList />} />
         <Route path="/produit/:slug" element={<ProductDetail />} />
       </Routes>
     </BrowserRouter>
