30c9dc88d1572b93e2842443cbe13ece1a8a1aaa
Ryuuu69 <144459226+Ryuuu69@users.noreply.github.com>
2025-07-27 16:55:36 +0200

    Updated components.js

--- frontend/src/components.js
+++ frontend/src/components.js
@@ -188,7 +188,9 @@ export const ProductCard = ({ product }) => {
           {/* Add Button */}
           <button
         className="w-11 h-11 bg-black rounded-full flex items-center justify-center"
-        onClick={() => navigate(`/produit/${product.id}`)}
+        onClick={() => {
+   if (product.id === 11) navigate('/big-cheese');
+ }}
           >
             <svg className="w-5 h-5 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 4v16m8-8H4" />
