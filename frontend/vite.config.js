9c84c384464189ca027971d6e6d1182ebe8d2b90
Ryuuu69 <144459226+Ryuuu69@users.noreply.github.com>
2025-07-27 09:16:33 +0200

    Updated vite.config.js

---
+++ frontend/vite.config.js
@@ -0,0 +1,6 @@
+// vite.config.js
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  publicDir: 'images'    // maintenant Vite servira ton dossier images comme dossier public
+})
