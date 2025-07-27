60d1ce0e2f472a8fc935081744b425a71aeca3a9
Ryuuu69 <144459226+Ryuuu69@users.noreply.github.com>
2025-07-27 16:34:05 +0200

    Updated components.js

--- frontend/src/components.js
+++ frontend/src/components.js
@@ -189,7 +189,7 @@ export const ProductCard = ({ product }) => {
           <button
         className="w-11 h-11 bg-black rounded-full flex items-center justify-center"
         onClick={() => navigate(`/produit/${product.id}`)}
-      >
+          >
             <svg className="w-5 h-5 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 4v16m8-8H4" />
             </svg>
