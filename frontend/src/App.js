d26df68be8674f72a26fae5c60b6b8f040120514
Ryuuu69 <144459226+Ryuuu69@users.noreply.github.com>
2025-07-27 17:08:25 +0200

    Updated App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,14 @@
-// frontend/src/App.js
-import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
-import Home           from './Home';            // ou ProductList si c'est ton composant d'accueil
-import Produits       from './Produits';        // ta page de listing
-import ProductDetail  from './ProductDetail';   // le composant générique
-import BigCheeseDetail from './BigCheeseDetail'; // optionnel, page isolée
+import ProductList   from './ProductList';
+import ProductDetail from './ProductDetail';   // ← (point 2)
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* Accueil (sélection de ville ou ProductList) */}
-        <Route path="/" element={<Home />} />
-
-        {/* Liste de tous les produits */}
-        <Route path="/produits" element={<Produits />} />
-
-        {/* Détail générique d’un produit */}
+        <Route path="/"           element={<ProductList />} />
+        {/* UNE SEULE page détail, paramètre :id */}
         <Route path="/produit/:id" element={<ProductDetail />} />
-
-        {/* Page isolée Big Cheese (pour test ou page dédiée) */}
-        <Route path="/big-cheese" element={<BigCheeseDetail />} />
       </Routes>
     </BrowserRouter>
   );
