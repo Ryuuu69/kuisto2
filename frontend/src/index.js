76ab33f5ac05aac7dba6d1b39287e70925a3279e
Ryuuu69 <144459226+Ryuuu69@users.noreply.github.com>
2025-07-27 11:31:46 +0200

    Updated index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,8 @@
-// frontend/src/index.js
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import './index.css'; // ou ton CSS
+import './index.css';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
@@ -13,4 +12,5 @@ root.render(
     <App />
   </BrowserRouter>
 );
+
 
