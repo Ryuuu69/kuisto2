5a0de6ba657f0444e4847ba35db37a0c99387617
Ryuuu69 <144459226+Ryuuu69@users.noreply.github.com>
2025-07-27 09:17:22 +0200

    Updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,6 @@
-// vite.config.js
-import { defineConfig } from 'vite';
+/// vite.config.js
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-  root: '.',         // racine du projet
-  publicDir: 'images', // dossier des fichiers statiques (SVG, PNG…)
-  server: {
-    port: 3000,       // accès via http://localhost:3000
-    open: true        // ouvre le navigateur automatiquement
-  }
-});
+  publicDir: 'images'    // maintenant Vite servira ton dossier images comme dossier public
+})
