b74eeba9b0d1eb6f4faa7753d4cfb2e9823dba02
Ryuuu69 <144459226+Ryuuu69@users.noreply.github.com>
2025-07-27 21:50:35 +0200

    Updated App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,12 @@
-// frontend/src/App.js
 import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import ProductDetail from './ProductDetail';
 
 export default function App() {
   return (
-    // basename fait que React gère uniquement /produit/*
     <BrowserRouter basename="/produit">
       <Routes>
-        {/* /produit/:slug */}
         <Route path=":slug" element={<ProductDetail />} />
-        {/* Redirige tout autre vers / */}
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
