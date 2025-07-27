ad71735f16764f80eb6ed314f80f05fd582b5317
Ryuuu69 <144459226+Ryuuu69@users.noreply.github.com>
2025-07-27 20:47:48 +0200

    Updated App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,17 @@
 // frontend/src/App.js
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import ProductDetail from './ProductDetail';
 
 export default function App() {
   return (
-    // On ne gère plus la racine "/" ici, uniquement les URLs "/produit/:slug"
+    // basename fait que React gère uniquement /produit/*
     <BrowserRouter basename="/produit">
       <Routes>
-        {/* La page détail dynamique sera servie sous "/produit/:slug" */}
+        {/* /produit/:slug */}
         <Route path=":slug" element={<ProductDetail />} />
+        {/* Redirige tout autre vers / */}
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
   );
