ae43fd80dab7367c8ca04efc4c429c3abe830f96
Ryuuu69 <144459226+Ryuuu69@users.noreply.github.com>
2025-07-27 09:17:07 +0200

    Deleted vite.config.js

--- frontend/vite.config.js
+++
@@ -1,6 +0,0 @@
-// vite.config.js
-import { defineConfig } from 'vite'
-
-export default defineConfig({
-  publicDir: 'images'    // maintenant Vite servira ton dossier images comme dossier public
-})
