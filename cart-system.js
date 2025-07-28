0cdb1af605f9b969e244952fa6a1fe00c5990e89
Ryuuu69 <144459226+Ryuuu69@users.noreply.github.com>
2025-07-28 16:25:44 +0200

    Fix syntax error in cart-system.js

--- cart-system.js
+++ cart-system.js
@@ -224,5 +224,5 @@ document.addEventListener('DOMContentLoaded', () => {
   cartSystem = new CartSystem();
   window.cartSystem = cartSystem;
 });
-window.CartSystem = CartSystem; // <--- PAS D'ESPACE, PAS DE POINT, PAS DE VIRGULE, RIEN
+window.CartSystem = CartSystem;
 } // <--- Fin du else
